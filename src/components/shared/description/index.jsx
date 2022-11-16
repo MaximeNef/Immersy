001777be56e92b0aa37b4e3523bdd6a358958c45
Nef <maximenef1@gmail.com>
2022-11-16 12:24:52 +0100

    create composant Description

--- src/components/shared/description/index.jsx
+++ src/components/shared/description/index.jsx
@@ -1,6 +1,12 @@
-import Container from '../container'
-
-const Description = () => {
-  return <Container>description</Container>
+const Description = (props) => {
+  return (
+    <div>
+      <p
+        className={`${props.color} text-left font-light leading-6 tracking-[0.03em]`}
+      >
+        {props.text}
+      </p>
+    </div>
+  )
 }
 export default Description
