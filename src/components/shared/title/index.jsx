56f50250a7418ffaf76655a9377e9c7d011acc7e
fel <felicien.dehertogh@protonmail.com>
2022-11-21 15:07:11 +0100

    create Besoins component

--- src/components/shared/title/index.jsx
+++ src/components/shared/title/index.jsx
@@ -1,7 +1,7 @@
-const Title = ({ color, mainTitle, subTitle }) => {
+const Title = ({ color, fontWeight, mainTitle, subTitle }) => {
   return (
     <div>
-      <h2 className={`${color} text-xl`}>{mainTitle}</h2>
+      <h2 className={`${color} text-xl ${fontWeight}`}>{mainTitle}</h2>
       <h2 className={`${color} text-xl font-thin`}>{subTitle}</h2>
     </div>
   )
