b8895c3b241f3c8ab05bd53de23e0e5130ec6d65
fel <felicien.dehertogh@protonmail.com>
2022-12-16 14:27:32 +0100

    responsive realisationCards

--- src/components/shared/realisationCards/index.jsx
+++ src/components/shared/realisationCards/index.jsx
@@ -4,13 +4,13 @@ import MyImage from '../myimage'
 
 const RealisationCards = () => {
   return (
-    <Container className="space-y-6">
-      <Container className="bg-white relative h-[225px] w-[350px] mx-auto">
+    <Container className="space-y-6 md:flex md:flex-row ">
+      <Container className="bg-white relative h-[225px] w-[350px] m-auto">
         <Link href="/">
           <MyImage src="/assets/visite.svg" layout="fill" className="h-fit" />
         </Link>
       </Container>
-      <Container className="bg-white relative h-[225px] w-[350px] mx-auto">
+      <Container className="bg-white relative h-[225px] w-[350px] m-auto">
         <Link href="/">
           <MyImage src="/assets/visite2.svg" layout="fill" className="h-fit" />
         </Link>
