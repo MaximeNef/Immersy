f650c56f69ae008e42b3d9ecd4e235d619dbbb87
Nef <maximenef1@gmail.com>
2022-12-22 10:11:41 +0100

    add Uxwizz

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,24 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* UXWIZZ script  */}
+
+      <Script id="uxwizz">
+        UST_CT = []; UST ={' '}
+        {'s: Date.now(), addTag: function(tag) {UST_CT.push(tag)} '}
+        ;UST.addEvent = UST.addTag;
+      </Script>
+      <Script
+        src="https://stats.listri.digital/server/ust.min.js?v=4.5.0"
+        async
+      ></Script>
+      {/* UXWIZZ script  */}
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
