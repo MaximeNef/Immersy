f962e9cfca951f1f2f9537bb0dacc3d2e633f396
fel <felicien.dehertogh@protonmail.com>
2022-12-22 14:28:08 +0100

    fix uxwizz script

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,9 +5,9 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script id="uxwizz">
-        UST_CT = []; UST =
+        UST_CT = []; UST = &quot;
         {'s: Date.now(), addTag: function(tag) {UST_CT.push(tag)}'}
-        ;UST.addEvent = UST.addTag;
+        &quot; ;UST.addEvent = UST.addTag;
       </Script>
       <Script
         src="https://stats.listri.digital/server/ust.min.js?v=4.5.0"
