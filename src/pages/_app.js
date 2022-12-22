614a08c45069d9f27d61309987634b8d01f39b68
Nef <maximenef1@gmail.com>
2022-12-22 10:31:44 +0100

    uxwizz

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,17 +5,15 @@ export default function MyApp({ Component, pageProps }) {
   return (
     <>
       {/* UXWIZZ script  */}
-
-      <Script id="uxwizz-Immersy">
-        UST_CT = []; UST =`
-        {'s: Date.now(), addTag: function(tag) {UST_CT.push(tag)}'}`
+      <Script id="uxwizz">
+        UST_CT = []; UST =
+        {'s: Date.now(), addTag: function(tag) {UST_CT.push(tag)}'}
         ;UST.addEvent = UST.addTag;
       </Script>
       <Script
         src="https://stats.listri.digital/server/ust.min.js?v=4.5.0"
         async
       ></Script>
-      {/* UXWIZZ script  */}
       <Component {...pageProps} />
     </>
   )
